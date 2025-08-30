0882245098fcc626cfed7be97c58ae78252733f1
jIab-b <120071162+sammaccc@users.noreply.github.com>
2025-08-30 21:28:26 +1000

    df

--- modules/diffusion/register_types.cpp
+++ modules/diffusion/register_types.cpp
@@ -1,4 +1,11 @@
-#include "register_types.h"
+#pragma once
+
+#include "modules/register_module_types.h"
+
+void initialize_diffusion_module(ModuleInitializationLevel p_level);
+void uninitialize_diffusion_module(ModuleInitializationLevel p_level);
+
+
 
 #include "core/config/project_settings.h"
 #include "core/object/class_db.h"
